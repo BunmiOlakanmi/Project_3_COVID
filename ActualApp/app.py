5820dcc847cf62f4e7f1862d21368da649b7075f
Bunmi <71471355+BunmiOlakanmi@users.noreply.github.com>
2021-02-06 13:27:22 -0600

    Update app.py

--- ActualApp/app.py
+++ ActualApp/app.py
@@ -12,15 +12,15 @@ from bson import json_util
 
 app = Flask(__name__)
 
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
 #connect to MongoDB Atlas database
 client= MongoClient("mongodb://dbUser:dbUser@cluster0-shard-00-00.lfpww.mongodb.net:27017,cluster0-shard-00-01.lfpww.mongodb.net:27017,cluster0-shard-00-02.lfpww.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-za5qq0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.covid_db
-=======
+#=======
 #app.config['MONGO_DATABASE_URI'] ="mongodb://dbUser:dbUser@cluster0-shard-00-00.lfpww.mongodb.net:27017,cluster0-shard-00-01.lfpww.mongodb.net:27017,cluster0-shard-00-02.lfpww.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-za5qq0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 #mongo = PyMongo(app)
->>>>>>> Stashed changes
+#>>>>>>> Stashed changes
 
 #Front end route
 @app.route('/')
@@ -30,17 +30,17 @@ def index():
 #service route
 @app.route("/api")
 def stringencyRoute():
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
     data= db.collection_stringency.find()
     json_docs = []
     for doc in data:
         json_doc = json.dumps(doc, default=json_util.default)
         json_docs.append(json_doc)
     return jsonify(json_docs)
-=======
+#=======
     return "welcome" 
     #return jsonify(data)
->>>>>>> Stashed changes
+#>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     app.run(debug=True)

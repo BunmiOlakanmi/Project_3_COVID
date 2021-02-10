449990d0bc6d8ba3c89644386f0a2cec64b0195b
Bunmi <71471355+BunmiOlakanmi@users.noreply.github.com>
2021-02-10 17:59:54 -0600

    covid-deaths.js

--- ActualApp/static/js/covid-deaths.js
+++ ActualApp/static/js/covid-deaths.js
@@ -1,20 +1,19 @@
-//function buildCharts(sampleId){
-  d3.json('/api_covid_infection').then(function(response){
-    console.log(response)
-    var country = response.Country;
-    var totalCases = response.Total_Cases;
-    var totalDeaths = response.Total_Deaths;
-    var totalRecovered = response.Total_Recovered;
-    var activeCases = response.Active_Cases;
-    var totalTests = response.Total_Tests;
-    var population = response.Population;
 
+
+
+function pietotalCases(){
+  d3.json('/api_covid_infection').then(function(response){
+    var country = response.map(covid=>covid.Country);
+    var totalCases = response.map(covid=>covid.Total_Cases);
+    // console.log(country);
+    
     var totalCasesData = [{
       values: totalCases,
       labels: country,
       //domain: {column: 0},
       name: 'Total Cases',
       hoverinfo: 'labels + values + name',
+      textinfo: 'none',
       hole: .4,
       type: 'pie'
     }];
@@ -24,136 +23,235 @@
       annotations: 
         {
           font: {
-            size: 20
-          },
+          size: 20
+        },
           showarrow: false,
-          text: 'COVID-19',
-          x: 0.17,
-          y: 0.5
-        }
-      };
-      var totalDeathsData = [{
-        values: totalDeaths,
-        labels: country,
-        //domain: {column: 0},
-        name: 'Total Deaths',
-        hoverinfo: 'labels + values + name',
-        hole: .4,
-        type: 'pie'
-      }];
-  
-      var totalDeathsLayout = {
-        title: 'Total COVID-19 Deaths by Country',
-        annotations: 
-          {
-            font: {
-              size: 20
-            },
-            showarrow: false,
-            text: 'COVID-19',
-            x: 0.17,
-            y: 0.5
-          }
-        };
-        var totalRecoveredData = [{
-          values: totalRecovered,
-          labels: country,
-          //domain: {column: 0},
-          name: 'Total Recovered',
-          hoverinfo: 'labels + values + name',
-          hole: .4,
-          type: 'pie'
-        }];
-    
-        var totalRecoveredLayout = {
-          title: 'Total COVID-19 Cases Recovered by Country',
-          annotations: 
-            {
-              font: {
-                size: 20
-              },
-              showarrow: false,
-              text: 'COVID-19',
-              x: 0.17,
-              y: 0.5
-            }
-          };
-
-          var activeCasesData = [{
-            values: activeCases,
-            labels: country,
-            //domain: {column: 0},
-            name: 'Active Cases',
-            hoverinfo: 'labels + values + name',
-            hole: .4,
-            type: 'pie'
-          }];
-      
-          var totalCasesLayout = {
-            title: 'Total COVID-19 Active Cases by Country',
-            // annotations: 
-            // {
-            //   font: {
-            //     size: 20
-            //   },
-            //   showarrow: false,
-            //   text: 'COVID-19',
-            //   x: 0.17,
-            //   y: 0.5
-            // }
-          };
-    Plotly.newPlot("pie", totalCasesData, totalCasesLayout);
-    Plotly.newPlot("pie", totalDeathsData, totalDeathsLayout);
-    Plotly.newPlot("pie", totalRecoveredData, totalRecoveredLayout);
-    Plotly.newPlot("pie", activeCasesData, activeCasesLayout);
-
-  });
-
-  // Get the above data to populate left hand side html div (to provide textual data besides the chart)
-  var panel = d3.select("#country-data");
-    panel.html("");
-    panel.append("p").text(`country: ${country}`);
-    panel.append("p").text(`Total Cases: ${totalCases}`);
-    panel.append("p").text(`Total Deaths: ${totalDeaths}`);
-    panel.append("p").text(`Total Recovered: ${totalRecovered}`);
-    panel.append("p").text(`Active Cases: ${activeCases}`);
-    panel.append("p").text(`Total Tests: ${totalTests}`);
-    panel.append("p").text(`Population: ${population}`);
-
-    // Data for the Donut chart
-    
-
-  // Plot the donut chart
-        
-// };
-
-// Function to select the country that the donut chart will be built
-function optionChanged(selectedCountry){
-    buildCharts(selectedCountry);
-  }
-
-// Function to initiate the program. 
-function init() {
-//     //Read the json file and retrieve the data from samples 
-    d3.json("/api_covid_infection").then((data) => {
-//       //var samples = data.samples;
-      var firstSampleId  = reponse.Country;
-      buildCharts(firstSampleId);  
-//       // var metaData = data.metadata;
-//       // console.log(metaData);
-//       var firstCountry = samples[0].country;
-//       console.log(samples[0]);
-//       // Assign the value of the dropdown menu option to a variable
-      var dropdownMenu = d3.select("#selDataset");
-//       // Initialize an empty array for the sample data
-      var ids = [];
-      ids = response.Country;
-      console.log(ids);
-      for(i = 0; i<=ids.length; i++){
-        var sampleCountry = dropdownMenu.append("option").text(ids[i]);
+        text: 'COVID-19',
+        x: 0.17,
+        y: 0.5
       }
-    });
-  };
-  init();
+    };
+  Plotly.newPlot("pie", totalCasesData, totalCasesLayout);
+  });
+}
 
-  
+
+function pietotalDeaths(){
+  d3.json('/api_covid_infection').then(function(response){
+    var country = response.map(covid=>covid.Country);
+    var totalDeaths = response.map(covid=>covid.Total_Deaths);
+    // console.log(country);
+   
+    var totalDeathsData = [{
+      values: totalDeaths,
+      labels: country,
+      //domain: {column: 0},
+      name: 'Total Deaths',
+      hoverinfo: 'labels + values + name',
+      textinfo: 'none',
+      hole: .4,
+      type: 'pie'
+    }];
+
+    var totalDeathsLayout = {
+      title: 'Total COVID-19 Deaths by Country',
+      annotations: 
+        {
+          font: {
+          size: 20
+        },
+          showarrow: false,
+        text: 'COVID-19',
+        x: 0.17,
+        y: 0.5
+      }
+    };
+  Plotly.newPlot("pie", totalDeathsData, totalDeathsLayout);
+  });
+}
+
+
+function pieactiveCases(){
+  d3.json('/api_covid_infection').then(function(response){
+    var country = response.map(covid=>covid.Country);
+    var activeCases = response.map(covid=>covid.Active_Cases);
+    // console.log(country);
+    
+    var activeCasesData = [{
+      values: activeCases,
+      labels: country,
+      //domain: {column: 0},
+      name: 'Active Cases',
+      hoverinfo: 'labels + values + name',
+      textinfo: 'none',
+      hole: .4,
+      type: 'pie'
+    }];
+
+    var activeCasesLayout = {
+      title: 'Active COVID-19 Cases by Country',
+      annotations: 
+        {
+          font: {
+          size: 20
+        },
+          showarrow: false,
+        text: 'COVID-19',
+        x: 0.17,
+        y: 0.5
+      }
+    };
+  Plotly.newPlot("pie", activeCasesData, activeCasesLayout);
+  });
+}
+
+
+function pietotalRecovered(){
+  d3.json('/api_covid_infection').then(function(response){
+    var country = response.map(covid=>covid.Country);
+    var totalRecovered = response.map(covid=>covid.Total_Recovered);
+    // console.log(country);
+    
+    var totalRecoveredData = [{
+      values: totalRecovered,
+      labels: country,
+      //domain: {column: 0},
+      name: 'Total Recovered',
+      hoverinfo: 'labels + values + name',
+      textinfo: 'none',
+      hole: .4,
+      type: 'pie'
+    }];
+
+    var totalRecoveredLayout = {
+      title: 'Total COVID-19 Recovered by Country',
+      annotations: 
+        {
+          font: {
+          size: 20
+        },
+          showarrow: false,
+        text: 'COVID-19',
+        x: 0.17,
+        y: 0.5
+      }
+    };
+  Plotly.newPlot("pie", totalRecoveredData, totalRecoveredLayout);
+  });
+}
+
+
+function pietotalTests(){
+  d3.json('/api_covid_infection').then(function(response){
+    var country = response.map(covid=>covid.Country);
+    var totalTests = response.map(covid=>covid.Total_Tests);
+    // console.log(country);
+    
+    var totalTestsData = [{
+      values: totalTests,
+      labels: country,
+      //domain: {column: 0},
+      name: 'Total Tests',
+      hoverinfo: 'labels + values + name',
+      textinfo: 'none',
+      hole: .4,
+      type: 'pie'
+    }];
+
+    var totalTestsLayout = {
+      title: 'Total COVID-19 Tests by Country',
+      annotations: 
+        {
+          font: {
+          size: 20
+        },
+          showarrow: false,
+        text: 'COVID-19',
+        x: 0.17,
+        y: 0.5
+      }
+    };
+  Plotly.newPlot("pie", totalTestsData, totalTestsLayout);
+  });
+}
+
+
+function piePopulation(){
+  d3.json('/api_covid_infection').then(function(response){
+    var country = response.map(covid=>covid.Country);
+    var population = response.map(covid=>covid.Population);
+    console.log(country);
+    var populationData = [{
+      values: population,
+      labels: country,
+      //domain: {column: 0},
+      name: 'Population',
+      hoverinfo: 'labels + values + name',
+      textinfo: 'none',
+      hole: .4,
+      type: 'pie'
+    }];
+
+    var populationLayout = {
+      title: 'Population by Country',
+      annotations: 
+        {
+          font: {
+          size: 20
+        },
+          showarrow: false,
+        text: 'COVID-19',
+        x: 0.17,
+        y: 0.5
+      }
+    };
+  Plotly.newPlot("pie", populationData, populationLayout);
+  });
+}
+
+function optionChanged(selectedData){
+  if (selectedData =="total_cases"){
+    var panel = d3.select("#pie");
+    panel.html("");
+    pietotalCases();
+  } 
+  else if (selectedData =="total_deaths"){
+    var panel = d3.select("#pie");
+    panel.html("");
+    pietotalDeaths();
+  }
+  else if (selectedData == "active_cases"){
+    var panel = d3.select("#pie");
+    panel.html("");
+    pieactiveCases();
+  }
+  else if(selectedData == "total_recovered"){
+    var panel = d3.select("#pie");
+    panel.html("");
+    pietotalRecovered();
+  }
+  else if (selectedData == "total_tests"){
+    var panel = d3.select("#pie");
+    panel.html("");
+    pietotalTests();
+  }
+  else if(selectedData == "population"){
+    var panel = d3.select("#pie");
+    panel.html("");
+    piePopulation();
+  }
+}
+
+function init() {
+  var dropdownValues= ["total_cases", "total_deaths", "active_cases", "total_recovered", "total_tests", "population"];
+  var dropdownMenu = d3.select("#selDataset");
+  dropdownValues.forEach((sample)=>{
+    dropdownMenu.append("option")
+    .text(sample)
+    .property("value", sample);
+  })
+}
+init();
+
+
+

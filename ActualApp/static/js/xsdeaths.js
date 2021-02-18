a72d858adb63db5af9de97b5387a74d44bb6bef5
nihaljoshua <70868581+nihaljoshua@users.noreply.github.com>
2021-02-17 21:57:16 -0500

    Update xsdeaths.js

--- ActualApp/static/js/xsdeaths.js
+++ ActualApp/static/js/xsdeaths.js
@@ -1,155 +1,136 @@
-var svgWidth = 960;
-var svgHeight = 600;
-padding = 100;
-
-
-leftMargin=70
-topMargin=30
-bottomMargin=40
-rightMargin=40
-
-var width = svgWidth - leftMargin - rightMargin;
-var height = svgHeight - topMargin - bottomMargin;
-
-
-d3.json('/api_unemployment').then(function(unemploymentdata) {
-
-    var parseTime = d3.timeParse("%B-%d-%Y");
-
-    unemploymentdata.forEach(function (data) {
-        data.Date = parseTime(data.Date);
-        data.Unemployment_Rate = +data.Unemployment_Rate;
-        data.Country = data.Country;
-
+function pietotaldeaths(){
+    d3.json("/api_excessdeaths").then(function(response){
+      var country = response.map(covid=>covid.Country);
+      var deaths = response.map(covid=>covid.deaths);
+      // console.log(country);
+      
+      var totaldeathsdata = [{
+        values: totaldeaths,
+        labels: country,
+        //domain: {column: 0},
+        name: 'Total Deaths',
+        hoverinfo: 'labels + values + name',
+        textinfo: 'none',
+        hole: .4,
+        type: 'pie'
+      }];
+  
+      var totaldeathsLayout = {
+        title: 'Total COVID-19 Deaths by Country',
+        annotations: 
+          {
+            font: {
+            size: 20
+          },
+            showarrow: false,
+          text: 'COVID-19',
+          x: 0.17,
+          y: 0.5
+        }
+      };
+    Plotly.newPlot("pie", totaldeathsdata, totaldeathsLayout);
     });
-
-// define the x scale (horizontal)
-var mindate = new Date(2020,02,28),
-    maxdate = new Date(2020,09,01);
-
-
-// scale xAxis
-var xScale = d3.scaleTime()
-    .domain([mindate, maxdate])
-    .range([leftMargin, 900])
-
-// scale yAxis
-var yScale=d3.scaleLinear()
-    .domain([0, 25])
-    .range([600, padding]);
-
-xAxis = d3.axisBottom(xScale)
-    .tickFormat(d3.timeFormat("%B-%d-%Y"));
-
-d3.select("svg")
-    .append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0,620)")
-    .call(xAxis)
-    .append("text")
-    .attr("x", (900+70)/2) 
-    .attr("y", "50")
-    .text("April 2020 to October 2020")
-
-yAxis = d3.axisLeft()
-    .scale(yScale)
-    .ticks(12)
-
-d3.select('svg')
-    .append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(${leftMargin},20)`) //use variable in translate
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("x", "-190")
-    .attr("y", "-40")
-    .attr("text-anchor", "end")
-    .text("Unemployment Rates Worldwide (%)")
-
-var sumstat = d3.nest() 
-    .key(d => d.Country)
-    .entries(unemploymentdata);
-
-console.log(sumstat)
-
-//set color pallete for different vairables
-var Countryname = sumstat.map(d => d.key) 
-var color = d3.scaleOrdinal().domain(Countryname).range(colorbrewer.Set2[6])
-
-//select path - three types: curveBasis,curveStep, curveCardinal
-d3.select("svg")
-    .selectAll(".line")
-    .append("g")
-    .attr("class", "line")
-    .data(sumstat)
-    .enter()
-    .append("path")
-    .attr("d", function (d) {
-        return d3.line()
-            .x(d => xScale(d.Time_dt))
-            .y(d => yScale(d.Unemployment_Rate)).curve(d3.curveLinear)
-            (d.values)
-    })
-    .attr("fill", "none")
-    .attr("stroke", d => color(d.key))
-    .attr("stroke-width", 2)
+  }
+  
+  
+  function pieexpecteddeaths(){
+    d3.json("/api_excessdeaths").then(function(response){
+      var country = response.map(covid=>covid.Country);
+      var totalexpecteddeaths = response.map(covid=>covid.expected_deaths);
+      // console.log(country);
+     
+      var totalExpectedDeaths = [{
+        values: totalDeaths,
+        labels: country,
+        //domain: {column: 0},
+        name: 'Total Expected Deaths',
+        hoverinfo: 'labels + values + name',
+        textinfo: 'none',
+        hole: .4,
+        type: 'pie'
+      }];
+  
+      var totalExpectedDeathsLayout = {
+        title: 'Total COVID-19 Deaths by Country',
+        annotations: 
+          {
+            font: {
+            size: 20
+          },
+            showarrow: false,
+          text: 'COVID-19',
+          x: 0.17,
+          y: 0.5
+        }
+      };
+    Plotly.newPlot("pie", totalExpectedDeaths, totalExpectedDeathsLayout);
+    });
+  }
+  
+  
+  function pieexcessdeaths(){
+    d3.json("/api_excessdeaths").then(function(response){
+      var country = response.map(covid=>covid.Country);
+      var excessdeaths = response.map(covid=>covid.excess_deaths);
+      // console.log(country);
+      
+      var excessdeathsData = [{
+        values: activeCases,
+        labels: country,
+        //domain: {column: 0},
+        name: 'Active Cases',
+        hoverinfo: 'labels + values + name',
+        textinfo: 'none',
+        hole: .4,
+        type: 'pie'
+      }];
+  
+      var excessdeathsLayout = {
+        title: 'Active COVID-19 Cases by Country',
+        annotations: 
+          {
+            font: {
+            size: 20
+          },
+            showarrow: false,
+          text: 'COVID-19',
+          x: 0.17,
+          y: 0.5
+        }
+      };
+    Plotly.newPlot("pie", excessdeathssData, excessdeathsLayout);
+    });
+  }
+  
+  
+  
+  
+  function optionChanged(selectedData){
+    if (selectedData =="Total Deaths"){
+      var panel = d3.select("#pie");
+      panel.html("");
+      pietotaldeaths();
+    } 
+    else if (selectedData =="Expected Deaths"){
+      var panel = d3.select("#pie");
+      panel.html("");
+      ppieexpecteddeaths();
+    }
+    else if (selectedData == "Excess Deaths"){
+      var panel = d3.select("#pie");
+      panel.html("");
+      pieexcessdeaths();
+    }
     
-
-//append title
-d3.select("svg")
-    .append("text")
-    .attr("x", 500)
-    .attr("y", 70)
-    .attr("text-anchor", "middle")
-    .text("Country-wise breakdown of Unemployment due to COVID-19")
-    .style("fill", "black")
-    .style("font-size", 20)
-    .style("font-family", "Arial Black")    
-
-//append circle 
-var circlesGroup = d3.select("svg")
-    .selectAll("circle")
-    .append("g")
-    .data(unemploymentdata)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d.Time_dt))
-    .attr("cy", d => yScale(d.Unemployment_Rate))
-    .attr("r", 6)
-    .style("fill", d => color(d.Country))
-
-
-//append legends
-var legend = d3.select("svg")
-    .selectAll('g.legend')
-    .data(sumstat)
-    .enter()
-    .append("g")
-    .attr("class", "legend")
-
-// Initialize tool tip 
-var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([40, 40])
-      .html(function(d) {
-        return (`${d.Country}<br>${d.Unemployment_Rate}`);
-      });
-
-    // Create tool tip
-    // ==============================
-    d3.select("svg")
-        .call(toolTip);
-
-    // Create event listeners to display and hide the tooltip
-    // ==============================
-    circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
+  }
+  
+  function init() {
+    var dropdownValues= ["Total Cases", "Total Deaths", "Active Cases", "Total Recovered", "Total Tests", "Population"];
+    var dropdownMenu = d3.select("#selDataset");
+    dropdownValues.forEach((sample)=>{
+      dropdownMenu.append("option")
+      .text(sample)
+      .property("value", sample);
     })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
-
-})
-
+  }
+  init();

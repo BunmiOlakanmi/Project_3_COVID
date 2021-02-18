ff32ab26099f7b24556078f11abd5d832ca724fb
nihaljoshua <70868581+nihaljoshua@users.noreply.github.com>
2021-02-18 00:02:42 -0500

    Upload final changes

--- ActualApp/static/js/xsdeaths.js
+++ ActualApp/static/js/xsdeaths.js
@@ -21,7 +21,7 @@ function pietotaldeaths(){
       }];
   
       var totaldeathsLayout = {
-        title: 'Total deaths due to COVID-19 by Country',
+        title: 'Total deaths due to COVID-19 by Jurisdiction',
         annotations: 
           {
             font: {
@@ -36,42 +36,7 @@ function pietotaldeaths(){
     Plotly.newPlot("pie", totaldeathsData, totaldeathsLayout);
     });
   }
-  
-  
-  function pieexpecteddeaths(){
-    d3.json("/api_excessdeaths").then(function(response){
-      var jurisdiction = response.map(covid=>covid.Jurisdiction);
-      var expecteddeaths = response.map(covid=>covid.expected_deaths);
-      // console.log(country);
-     
-      var expecteddeathsData = [{
-        values: expecteddeaths,
-        labels: jurisdiction,
-        //domain: {column: 0},
-        name: 'Total Expected Deaths',
-        hoverinfo: 'labels + values + name',
-        textinfo: 'none',
-        hole: .4,
-        type: 'pie'
-      }];
-  
-      var totalexpecteddeathslayout = {
-        title: 'Expected deaths due to COVID-19 by Country',
-        annotations: 
-          {
-            font: {
-            size: 20
-          },
-            showarrow: false,
-          text: 'COVID-19',
-          x: 0.17,
-          y: 0.5
-        }
-      };
-    Plotly.newPlot("pie", expecteddeathsData, totalexpecteddeathslayout);
-    });
-  }
-  
+    
   
   function pieexcessdeaths(){
     d3.json("/api_excessdeaths").then(function(response){
@@ -91,7 +56,7 @@ function pietotaldeaths(){
       }];
   
       var excessdeathsLayout = {
-        title: 'Excess deaths due to COVID-19 by Country',
+        title: 'Excess deaths due to COVID-19 by Jurisdiction',
         annotations: 
           {
             font: {
@@ -116,11 +81,7 @@ function pietotaldeaths(){
       panel.html("");
       pietotaldeaths();
     } 
-    else if (selectedData == "Expected Deaths"){
-      var panel = d3.select("#pie");
-      panel.html("");
-      pieexpecteddeaths();
-    }
+  
     else if (selectedData == "Excess Deaths"){
       var panel = d3.select("#pie");
       panel.html("");
@@ -130,7 +91,7 @@ function pietotaldeaths(){
   }
   
   function init() {
-    var dropdownValues= ["Total Deaths", "Expected Deaths", "Excess Deaths"];
+    var dropdownValues= ["Total Deaths", "Excess Deaths"];
     var dropdownMenu = d3.select("#selData");
     dropdownValues.forEach((sample)=>{
       dropdownMenu.append("option")
